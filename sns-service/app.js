a93a93e680fe3aad7e229062c27724ce9c313743
lgy <allowttt@naver.com>
2021-05-26 23:13:11 +0900

    sns pj 2021-05-26

--- sns-service/app.js
+++ sns-service/app.js
@@ -1,1 +1,65 @@
-//npx sequelize init -> 시퀄 구조 만들어준다.
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config();    //process.env에 config에 설정한 설정값이 들어간다.
+const pageRouter = require('./routes/page');
+
+const {sequelize} = require('./models');
+
+const app = express();
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+
+sequelize.sync({force: false})
+    .then(() => {
+        console.log('db연결 성공');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/img', express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+}));
+
+app.use('/', pageRouter);
+
+app.use((req, res, next) => {
+    console.log('404404404');
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    // res.status(err.status || 500);
+    // res.render('error');
+    res.status(err.status || 500).render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'),'번 포트에서 대기중!');
+});
