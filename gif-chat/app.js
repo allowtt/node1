aed32dc4ed46680953978897673193ec5e116aa6
lgy <allowttt@naver.com>
2021-08-23 19:15:37 +0900

    123444

--- gif-chat/app.js
+++ gif-chat/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');   //요청에대한거 기록 콘솔에 
+const morgan = require('morgan');   //요청에대한거 기록 콘솔에 111
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
